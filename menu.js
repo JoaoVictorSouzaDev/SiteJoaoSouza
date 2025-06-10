1483acb019cead20df4206b943cb815b4a4a5190
Joao Souza <joaovictorsouza3233@gmail.com>
2025-06-10 17:12:49 -0300

    Adição update

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
-let overlay =document.getElementById('overlay-menu')
+let overlay = document.getElementById('overlay-menu')
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -13,3 +13,78 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+class ForSubmit {
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(this.settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
+
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+
+    onSubmission(event) {
+        event.preventDefault();
+        this.formButton.disabled = true;
+        this.formButton.innerText = "Enviando...";
+    }
+
+    async sendForm(event) {
+        this.onSubmission(event);
+        try {
+            const response = await fetch(this.url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify(this.getFormObject()),
+            });
+
+            if (response.ok) {
+                alert(this.settings.sucessAlertMessage);
+                window.location.href = this.settings.redirectUrl;
+            } else {
+                const errorData = await response.text();
+                console.error("Erro na resposta do FormSubmit:", errorData);
+                alert(this.settings.errorAlertMessage);
+            }
+
+        } catch (error) {
+            console.error("Erro ao enviar formulário:", error);
+            alert(this.settings.errorAlertMessage);
+        } finally {
+            if (this.formButton) {
+                 this.formButton.disabled = false;
+                 this.formButton.innerText = "Enviar";
+            }
+        }
+    }
+
+    init() {
+        if (this.form) {
+            this.form.addEventListener("submit", this.sendForm);
+        }
+    }
+}
+
+const formSubmit = new ForSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    sucessAlertMessage: "Mensagem enviada com sucesso!",
+    errorAlertMessage: "Não foi possível enviar sua mensagem. Tente novamente.",
+    redirectUrl: "/",
+});
+
+formSubmit.init();
